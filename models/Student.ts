077de99c60a1b48cdd9a928bc38a524f34ee8bea
nitesh.asm <0911nitesh@gmail.com>
2026-04-19 00:05:12 +0530

    fix: email validation and update unique index

--- models/Student.ts
+++ models/Student.ts
@@ -20,11 +20,13 @@ const StudentSchema = new Schema<IStudent>(
     teacherId: { type: String, required: true, index: true, minlength: 1 },
     name: { type: String, required: true, trim: true, minlength: 1 },
     rollNo: { type: String, required: true, trim: true,  uppercase: true,minlength: 1 },
-    class: { type: String, required: true, trime: true,minlength: 1 },
-    email: {  type: String,
-      default: '',
-      trim: true,
-      match: [/^\S+@\S+\.\S+$/, 'Invalid email format'], },
+    class: { type: String, required: true, trim: true,minlength: 1 },
+    email: {  type: String, trim: true,
+      validate:{
+        validator: (value?: string) => !value || /^\S+@\S+\.\S+$/.test(value),
+        message: 'Invalid email format',
+      }
+      },
     phone: { type: String, default: '', trim: true },
     address: { type: String, default: '', trim: true },
     parentName: { type: String, default: '', trim: true },
@@ -33,6 +35,6 @@ const StudentSchema = new Schema<IStudent>(
   { timestamps: true }
 )
 
-StudentSchema.index({ teacherId: 1, class: 1 }, { unique: true });
+StudentSchema.index({ teacherId: 1, rollNo: 1 }, { unique: true });
 
 export const Student = models.Student ?? model<IStudent>('Student', StudentSchema)
